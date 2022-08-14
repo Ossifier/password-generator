5fa9249997ba2d80d273214b6ffcdb99b1efa522
OwenGMiller123 <111017418+OwenGMiller123@users.noreply.github.com>
2022-08-13 22:23:49 -0600

    Minor Code Refactor
    Did a slight bit of reworking for the code to give it a little more functionality.
    
    # NEW STUFF
    1. You can specify either upper case, lower case, or both. 
    2. More importantly, this code now builds a list from each generated password, which will be super important for integrating into the password storage part of the program later.
    
    Embarrassingly, these changes took way longer than I had hoped. In particular, I ended up drastically overcomplicating things before realizing that there were far simpler ways to accomplish what I wanted.
    
    Whoops!

--- passgen.py
+++ passgen.py
@@ -1,46 +1,48 @@
 import string
 import random
 
-alpha_characters = list(string.ascii_letters)
-digits = list(string.digits)
-special_characters = list("~!@#$%^&*")
-all_characters = list(string.ascii_letters + string.digits + "~!@#$%^&*")
+alpha_characters_up = list(string.ascii_uppercase)
+alpha_characters_lo = list(string.ascii_lowercase)
+numbers = list(string.digits)
+special_characters = list(string.punctuation)
+all_characters = list(string.ascii_letters + string.digits + string.punctuation)
 
 
 def generate_password():
-    length = int(input("Total Number of Characters: "))
 
-    alpha_char_count = int(input("Number of letters in password: "))
-    digits_count = int(input("Number of numbers in password: "))
-    special_char_count = int(input("Number of special characters in password: "))
     password_gen_count = int(input("Number of passwords you would like to generate: "))
 
-    total_char_count = alpha_char_count + digits_count + special_char_count
-
-    if total_char_count > length:
-        print("Character total count is greater than password length")
-    elif total_char_count < length:
-        print("Character total count is less than password length.")
+    alpha_char_count_lo = int(input("Number of lower case letters in password: "))
+    alpha_char_count_up = int(input("Number of UPPER case letter in password: "))
+    number_count = int(input("Number of numbers in password: "))
+    special_count = int(input("Number of special characters in password: "))
 
     password = []
+    password_list = []
 
-    for i in range(password_gen_count):
-        for x in range(alpha_char_count):
-            password.append(random.choice(alpha_characters))
-        for x in range(digits_count):
-            password.append(random.choice(digits))
-        for x in range(special_char_count):
+    for n in range(password_gen_count):
+        for i in range(alpha_char_count_up):
+            password.append(random.choice(alpha_characters_up))
+        for i in range(alpha_char_count_lo):
+            password.append(random.choice(alpha_characters_lo))
+        for i in range(number_count):
+            password.append(random.choice(numbers))
+        for i in range(special_count):
             password.append(random.choice(special_characters))
-        if total_char_count < length:
-            random.shuffle(all_characters)
-            for x in range(length - total_char_count):
-                password.append(random.choice(all_characters))
 
         random.shuffle(password)
-
-        print("".join(password))
+        password_string = "".join(password)
+        password_list.append(password_string)
 
         password = []
+    
+    return password_list
 
 
-generate_password()
+test_list = generate_password()
+
+for i in test_list:
+    print(i)
+
+print('\nList Length: ')
+print(len(test_list))
