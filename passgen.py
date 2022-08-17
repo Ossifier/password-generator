d834d5a0d12328d26c203c36b3ae751be9f93382
OwenGMiller123 <111017418+OwenGMiller123@users.noreply.github.com>
2022-08-16 21:31:48 -0600

    Added functions and other neat stuff.
    I went ahead and made a few changes to the functions and modules.
    
    #THINGS DONE:
    1. changed import random to import secrets. Although I have no aspirations for anyone using this project as a serious password generator, nonetheless it made sense to do it the right way and use methods that would be more cryptographically secure.
    2. More importantly, I added two new functions, one that generates a text dump based on the list generated from generate_passwords, and another function that can retrieve the data and put it back into a list for use later.
    3. Finally, I added an if __name__ = '__main__' so that you if you so choose, you can perform standalone tests using these files by themselves. This is important because undoubtedly I will be using these as modules for the main.py file once I get near completion for the project.

--- passgen.py
+++ passgen.py
@@ -1,5 +1,6 @@
 import string
-import random
+import secrets
+from random import shuffle
 
 alpha_characters_up = list(string.ascii_uppercase)
 alpha_characters_lo = list(string.ascii_lowercase)
@@ -8,41 +9,70 @@ special_characters = list(string.punctuation)
 all_characters = list(string.ascii_letters + string.digits + string.punctuation)
 
 
-def generate_password():
-
+def generate_passwords():
     password_gen_count = int(input("Number of passwords you would like to generate: "))
 
     alpha_char_count_lo = int(input("Number of lower case letters in password: "))
     alpha_char_count_up = int(input("Number of UPPER case letter in password: "))
     number_count = int(input("Number of numbers in password: "))
     special_count = int(input("Number of special characters in password: "))
+    print('')
 
     password = []
     password_list = []
 
     for n in range(password_gen_count):
         for i in range(alpha_char_count_up):
-            password.append(random.choice(alpha_characters_up))
+            password.append(secrets.choice(alpha_characters_up))
         for i in range(alpha_char_count_lo):
-            password.append(random.choice(alpha_characters_lo))
+            password.append(secrets.choice(alpha_characters_lo))
         for i in range(number_count):
-            password.append(random.choice(numbers))
+            password.append(secrets.choice(numbers))
         for i in range(special_count):
-            password.append(random.choice(special_characters))
+            password.append(secrets.choice(special_characters))
 
-        random.shuffle(password)
+        shuffle(password)
         password_string = "".join(password)
         password_list.append(password_string)
 
         password = []
-    
+
     return password_list
 
 
-test_list = generate_password()
+def create_txt_dump(password_list):
+    with open('passwords.txt', 'w') as f:
+        f.write('***Passwords***\n\n')
+        for password in password_list:
+            f.write("%s\n" % password)
+        print('')
+        print('List Dump Complete.')
 
-for i in test_list:
-    print(i)
 
-print('\nList Length: ')
-print(len(test_list))
+def retrieve_txt_dump():
+    fresh_test_list = []
+    list_from_file = open('passwords.txt', 'r').readlines()
+    del list_from_file[:2]
+
+    for i in list_from_file:
+        fresh_test_list.append(i.strip())
+    return fresh_test_list
+
+
+if __name__ == '__main__':
+
+    print('Run Directly, For Testing Purposes:\n')
+
+    test_list = generate_passwords()
+
+    for i in test_list:
+        print(i)
+
+    print('\nList Length: ')
+    print(len(test_list))
+
+    create_txt_dump(test_list)
+
+    final_test_list = retrieve_txt_dump()
+
+    print(list(final_test_list))
