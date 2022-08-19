6075f290a4103db5612dbfe6cbd2caf913874cb6
OwenGMiller123 <111017418+OwenGMiller123@users.noreply.github.com>
2022-08-19 11:17:08 -0600

    Added New Functions & Reorganized
    #THINGS DONE:
    1. Renamed "write_data" to "append_new_data" so the true purpose of the function is less vague and confusing.
    2. Added the "add_json_entry" function. This function uses previously defined functions to retrieve user inputs and write those inputs into a JSON file.
    3. Added the "delete_entry" function. This function, true to its name, deletes the key-value pair entered by the user.
    4. Added some section tags to make the code easier to navigate.
    
    #TO DO:
    1. Add extra code into the delete_entry function that returns an error if they key is not present within the dictionary. This will likely require some degree of re-writing.

--- write-to-json.py
+++ write-to-json.py
@@ -2,6 +2,11 @@ import os
 import json
 
 
+###>>>..................<<<###
+### FOLDER & FILE HANDLERS ###
+###>>>.................<<<####
+
+
 def get_folder_name():
     en_folder_name = input('Enter a folder name: ') + '/'
     print('Folder name: ' + en_folder_name)
@@ -9,7 +14,7 @@ def get_folder_name():
 
 
 def get_file_name(folder):
-    print('Directory Location ' + folder)
+    print('Directory Location: ' + folder)
     en_file_name = input('Enter a file name: ') + '.json'
     print('File name: ' + en_file_name)
     return folder + en_file_name
@@ -54,12 +59,14 @@ def file_check(password_folder, password_file):
         load_file(password_file)
 
 
-def get_user_info(password_file):
+###>>>...........................<<<###
+### DATA READING & WRITING HANDLERS ###
+###>>>..........................<<<####
 
-    file_data = load_file(password_file)
-    length = len(file_data)
 
-    en_name = input('Please enter a name for this login: ')
+def get_user_info():
+
+    en_name = input('Please enter an index name for this login: ')
     print('You entered: ' + en_name)
     en_username = input('Please enter your username for this login if applicable: ')
     print('You entered: ' + en_username)
@@ -68,30 +75,74 @@ def get_user_info(password_file):
     en_password = input('Please enter a password for this login: ')
     print('You entered: ' + en_password)
 
-    user_info = {length + 1: {"name": en_name, "username": en_username, "email": en_email, "password": en_password}}
+    user_info = {en_name: {'username': en_username, 'email': en_email, 'password': en_password}}
 
     return user_info
 
 
-def write_data(new_data, password_file):
-    with open(password_file, 'r+') as file:
+def append_new_data(new_data, file_path):
+    with open(file_path, 'r+') as file:
         file_data = json.load(file)
         file_data.update(new_data)
         file.seek(0)
         json.dump(file_data, file, indent=4)
 
 
+def add_json_entry():
+    user_data = get_user_info()
+    append_new_data(user_data, file_path)
+
+
+def delete_entry(file_path):
+    loaded_file = load_file(file_path)
+
+    print('List of Passwords:\n')
+    for key in loaded_file:
+        print(key)
+    print('')
+
+    key_to_delete = input('Enter the name of the entry you would like to delete: ')
+    print('')
+
+    for key in loaded_file:
+        if str(key) == str(key_to_delete):
+            del loaded_file[key_to_delete]
+            break
+        else:
+            pass
+
+    with open(file_path, 'w+') as file:
+        json.dump(loaded_file, file, indent=4)
+
+    print('Key Deleted Successfully: ' + key_to_delete + '\n')
+
+
+###>>>.......................<<<###
+### FUNCTION TESTING SUBSECTION ###
+###>>>......................<<<####
+
+
 if __name__ == '__main__':
 
     print('Run Directly, For Testing Purposes:\n')
 
-    folder_path = get_folder_name()
+    folder_path = 'password-folder/'
     print(folder_path)
-    file_path = get_file_name(folder_path)
+    file_path = 'password-folder/password-file.json'
     print(file_path)
 
     file_check(folder_path, file_path)
-    enter_data = get_user_info(file_path)
-    write_data(enter_data, file_path)
 
-    print(load_file(file_path))
+    continue_add = 'Y'
+    while continue_add == 'Y':
+        add_json_entry()
+        continue_add = input('Do you want to add another entry? (Y/N):' )
+
+    print('')
+
+    continue_del = 'Y'
+    while continue_del == 'Y':
+        delete_entry(file_path)
+        continue_del = input('Do you want to add another entry? (Y/N):' )
+
+    print('\nOperations Complete.')
